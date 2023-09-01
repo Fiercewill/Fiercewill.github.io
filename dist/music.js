9edc2149d7952800a67afc3a1cb3315a7422afd3
Fiercewill <3201935299@qq.com>
2023-09-01 16:47:36 +0800

    Site updated: 2023-09-01 16:47:36

--- dist/music.js
+++ dist/music.js
@@ -7,7 +7,7 @@ const ap = new APlayer({
         {
             name: '不想再爱不明白的爱', //如果只有一首歌，删掉这一块，如有更多歌曲按此格式逐渐往下添加
             artist: '戴爱玲',
-            url: 'https://m804.music.126.net/20230831134906/8f7d7982530b565519fef58b52149a40/jdyyaac/075c/030b/5258/0650a0aba64adf16fa98e2d479106b91.m4a?authSecret=0000018a4a0c7ab6061e0aaba50bbff5',
+            url: 'https://m804.music.126.net/20230901171047/f5ba6088245c3c46cfe6dd36150a6eb7/jdyyaac/075c/030b/5258/0650a0aba64adf16fa98e2d479106b91.m4a?authSecret=0000018a4feb7c811ef40aaba140032e',
             cover: '/image/love.jpg',
             theme: '#46718b'
         }
